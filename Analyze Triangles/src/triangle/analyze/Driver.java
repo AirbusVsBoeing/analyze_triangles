d29575d074d93473ff5b5d43a5e3bc87b831a6e5
Elijah Einstein <einsteine98@gmail.com>
2020-10-29 18:40:52 -0400

    adj list

---
+++ Analyze Triangles/src/triangle/analyze/Driver.java
@@ -0,0 +1,13 @@
+package triangle.analyze;
+
+public class Driver {
+
+	private Graph graph; 
+	
+	
+	public Graph readFile(String fileName) {
+		
+		return null;
+	}
+	
+}
---
+++ Analyze Triangles/src/triangle/analyze/Triangle.java
@@ -0,0 +1,23 @@
+package triangle.analyze;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Triangle {
+	
+	private Graph graph;
+	
+	public List<Graph> listTriangles(){
+		
+	
+		return null;
+	}
+	
+	
+	private Graph checkTriangle(int i, int j, int k) {
+		
+		return null;
+	}
+	
+	
+}
